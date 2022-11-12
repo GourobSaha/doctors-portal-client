41f2a1d77b7977078dbbed306fe67ae18db50ac1
Gourob Saha <gourobsaha009@gmail.com>
2022-11-12 16:31:55 +0600

    Footer Added

--- src/Pages/Home/Banner3.js
+++ src/Pages/Home/Banner3.js
@@ -10,7 +10,7 @@ const Banner3 = () => {
             <div className="hero">
                 <div className="hero-content p-0 flex-col lg:flex-row">
                     <img src={doctorImg} alt='doctor' className="-mt-36 hidden md:block lg:w-1/2 rounded-lg" />
-                    <div>
+                    <div className='p-3 md:p-0'>
                         <h3 className="text-2xl font-semibold text-primary">Appointment</h3>
                         <h1 className="text-4xl font-bold my-6 text-white">Make an appointment Today</h1>
                         <p className="py-6 text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import Banner2 from './Banner2';
 import Banner3 from './Banner3';
+import ContactUs from './ContactUs';
 import InfoCards from './InfoCards/InfoCards';
 import Services from './Services/Services';
 import Testimonial from './Testimonials/Testimonial';
@@ -15,6 +16,7 @@ const Home = () => {
             <Banner2></Banner2>
             <Banner3></Banner3>
             <Testimonial></Testimonial>
+            <ContactUs></ContactUs>
         </div>
     );
 };
