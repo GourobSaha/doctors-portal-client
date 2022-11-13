e5afdb799a2eb6b45a19c3d0b17507b55d98df0a
Gourob Saha <gourobsaha009@gmail.com>
2022-11-13 15:27:56 +0600

    Appointment Section Added

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,37 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
+import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({ selectedDate }) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
+    useEffect(() => {
+        fetch('appointmentOptions.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }, [])
+
     return (
         <div className='text-center my-12'>
             <h3 className='text-3xl text-secondary'>Available Appointments on {format(selectedDate, 'PP')}</h3>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-6 my-5'>
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        option={option}
+                        setTreatment={setTreatment}
+                    ></AppointmentOption>)
+                }
+            </div>
+            {treatment &&
+                <BookingModal
+                    selectedDate={selectedDate}
+                    treatment={treatment}
+                    setTreatment={setTreatment}
+                ></BookingModal>
+            }
         </div>
     );
 };
